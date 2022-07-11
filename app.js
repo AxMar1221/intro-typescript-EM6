607cdc66a7ec674db391ec5fcc5dd6551f869b94
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-10 20:46:31 -0500

    tipos

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+(() => {
+    let mensaje = 'Hola';
+    let numero = 123;
+    let booleano = false; //true
+    let hoy = new Date();
+    let cualquiercosa;
+    cualquiercosa = mensaje;
+    cualquiercosa = numero;
+    cualquiercosa = booleano;
+    cualquiercosa = hoy;
+    let spiderman = {
+        nombre: 'peter',
+        edad: 30
+    };
+    spiderman = {
+        nombre: 'Juan',
+        edad: 40
+    };
+})();
---
+++ typescript/tipos.ts
@@ -0,0 +1,23 @@
+(()=>{
+    let mensaje: string = 'Hola';
+    let numero: number = 123;
+    let booleano: boolean = false; //true
+    let hoy: Date = new Date();
+
+    let cualquiercosa;
+    cualquiercosa = mensaje;
+    cualquiercosa = numero;
+    cualquiercosa = booleano;
+    cualquiercosa = hoy;
+
+    let spiderman = {
+        nombre: 'peter',
+        edad: 30
+    };
+
+    spiderman = {
+        nombre: 'Juan',
+        edad: 40
+    }
+})();
+
