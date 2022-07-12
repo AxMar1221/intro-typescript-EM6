3824dff0ffef3a96f96d66374921ea0c377d3465
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-12 16:41:45 -0500

    practica2 app2

--- app.js
+++ app.js
@@ -3,10 +3,7 @@
     // Uso de Let y Const
     let nombre = 'Ricardo Tapia';
     let edad = 23;
-    const PERSONAJE = {
-        nombre: nombre,
-        edad: edad
-    };
+    const PERSONAJE = { nombre, edad };
     const batman = {
         nombre: 'Bruno Díaz',
         artesMarciales: ['Karate', 'Aikido', 'Wing Chun', 'Jiu-Jitsu']
@@ -18,7 +15,7 @@
     //       PODER  = opcional
     //       ARMA   = por defecto = 'arco'
     function getAvenger(nombre, poder, arma = 'arco') {
-        var mensaje;
+        let mensaje;
         if (poder) {
             mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
         }
@@ -34,4 +31,13 @@
     //   * altura
     // También un método que calcule el área  =  base * altura,
     // ese método debe de retornar un numero.
+    class Rectangulo {
+        constructor(base, altura) {
+            this.base = base;
+            this.altura = altura;
+        }
+        clacularArea() {
+            return this.base * this.altura;
+        }
+    }
 })();
--- app.ts
+++ app.ts
@@ -4,10 +4,7 @@
   let nombre = 'Ricardo Tapia';
   let edad = 23;
 
-  const PERSONAJE = {
-    nombre: nombre,
-    edad: edad
-  };
+  const PERSONAJE = { nombre, edad };
   // Cree una interfaz que sirva para validar el siguiente objeto
   interface Batman {
     nombre: string;
@@ -20,7 +17,7 @@
   }
 
   // Convertir esta funcion a una funcion de flecha
-  const resultadoDoble = ( a:number, b:number ) => ((a + b) * 2)
+  const resultadoDoble = ( a:number, b:number ):number => ((a + b) * 2)
   
 
   // Función con parametros obligatorios, opcionales y por defecto
@@ -30,7 +27,7 @@
   function getAvenger( nombre: string,
                        poder?: string, 
                        arma: string = 'arco' ){
-    var mensaje;
+    let mensaje;
     if( poder ){ 
       mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
     }else{
@@ -45,7 +42,13 @@
   //   * altura
   // También un método que calcule el área  =  base * altura,
   // ese método debe de retornar un numero.
+  class Rectangulo {
 
-
-
+    constructor( public base:number,
+                 public altura:number,
+                  ){}
+    clacularArea() {
+      return this.base * this.altura;
+    }
+  } 
 })();
