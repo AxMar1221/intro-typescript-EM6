5c6dcaa3a4d50a3d7937fae7a667a809940874e6
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-11 23:07:19 -0500

    clases

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 "use strict";
 (() => {
-    const enviarMision = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la misión`);
-    };
-    const regresarAlCuartel = (xmen) => {
-        console.log(`Regresando ${xmen.nombre} al cuartel`);
-    };
-    const wolverine = {
-        nombre: 'Logan',
-        edad: 60
-    };
-    enviarMision(wolverine);
-    regresarAlCuartel(wolverine);
+    class Avenger {
+        // nombre: string;
+        // equipo: string;
+        // nombreReal:  string;
+        // puedePelear: boolean;
+        // peleasGanadas: number;
+        constructor(nombre, equipo, nombreReal, puedePelear = true, peleasGanadas = 0) {
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
+        }
+    }
+    const antman = new Avenger('Capitan América', 'Capi', 'Steve');
+    console.log(antman);
 })();
+// class Avenger {
+//     nombre: string;
+//     equipo: string;
+//     nombreReal?:  string;
+//     puedePelear?: boolean;
+//     peleasGanadas?: number;
+//     constructor( nombre: string, equio: string) {
+//         this.nombre = nombre;
+//         this.equipo = equio;
+//     }
+// }
+// const antman: Avenger = new Avenger( 'Antman', 'Capi');
+// console.log( antman );
